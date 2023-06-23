bdd4bbd31f41aedcc4f1cec73e50c83d6b37dc3f
Admin <Admin@DESKTOP-N8H8ECD>
2023-06-23 11:58:16 +0530

    [pooja]: add practice project

---
+++ src/Framework/Demo.java
@@ -0,0 +1,13 @@
+package Framework;
+
+import java.io.FileInputStream;
+
+public class Demo 
+{
+   public static void main(String[] args) 
+   {
+//	   FileInputStream file=new FileInputStream("");
+//	   WorkB
+	   
+   }
+}
